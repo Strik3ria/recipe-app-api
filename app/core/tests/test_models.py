33846846b8a2e58dd3b40b5f5c33238be86fb413
Strik3ria <robert_coones@zohomail.com>
2022-03-18 15:09:25 -0500

    Added working list tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email="test@test.com", password="testpass"):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
@@ -9,8 +16,7 @@ class ModelTests(TestCase):
         password = "testpass123"
 
         user = get_user_model().objects.create_user(
-            email=email,
-            password=password
+            email=email, password=password
         )
 
         self.assertEqual(user.email, email)
@@ -25,7 +31,7 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test123')
+            get_user_model().objects.create_user(None, "test123")
 
     def test_create_superuser_successful(self):
         """Tests creating a superuser is successful"""
@@ -33,10 +39,15 @@ class ModelTests(TestCase):
         password = "testpass123"
 
         user = get_user_model().objects.create_superuser(
-            email=email,
-            password=password
+            email=email, password=password
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(user=sample_user(), name="Vegan")
+
+        self.assertEqual(str(tag), tag.name)
